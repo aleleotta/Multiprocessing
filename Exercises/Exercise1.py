18d6bb41c6b28499340f39555827aae60cbd9f26
aleleotta <114069815+aleleotta@users.noreply.github.com>
2023-11-18 00:45:14 +0100

    .

--- Exercises/Exercise1.py
+++ Exercises/Exercise1.py
@@ -1,16 +1,16 @@
 from multiprocessing import Process
 
-def suma(max):
+def sum(max):
     res = 0
     for i in range(1, max+1):
         res = res + i
     print(res)
 
-p1 = Process(target=suma, args=(10,))
-p2 = Process(target=suma, args=(15,))
-p3 = Process(target=suma, args=(25,))
-p4 = Process(target=suma, args=(100,))
-p5 = Process(target=suma, args=(156,))
+p1 = Process(target=sum, args=(10,))
+p2 = Process(target=sum, args=(15,))
+p3 = Process(target=sum, args=(25,))
+p4 = Process(target=sum, args=(100,))
+p5 = Process(target=sum, args=(156,))
 
 if __name__ == "__main__":
     p1.start()
--- Exercises/Exercise5.py
+++ Exercises/Exercise5.py
@@ -1,2 +1,24 @@
 from multiprocessing import Process
 
+def sum(min, max):
+    res = 0
+    if min > max:
+        min, max = max, min
+    for i in range(min, max+1):
+        res = res + i
+    print(res)
+
+p1 = Process(target=sum, args=(1, 10))
+p2 = Process(target=sum, args=(10, 55))
+p3 = Process(target=sum, args=(100, 1000))
+p4 = Process(target=sum, args=(13145, 55436))
+p5 = Process(target=sum, args=(100000000, 55075))
+
+if __name__ == "__main__":
+    p1.start()
+    p2.start()
+    p3.start()
+    p4.start()
+    p5.start()
+    p5.join()
+    print("All processes have been executed.")
