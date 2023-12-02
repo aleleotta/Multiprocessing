431997e7ef95d8d854bb8fdb7f8bf03d327ad601
aleleotta <114069815+aleleotta@users.noreply.github.com>
2023-12-02 02:01:17 +0100

    .

--- Exercises Part 2/Exercise2.py
+++ Exercises Part 2/Exercise2.py
@@ -1,1 +1,63 @@
 from multiprocessing import *
+from random import randint
+
+def randomIp(connection):
+    num1 = randint(0, 255)
+    num2 = randint(0, 255)
+    num3 = randint(0, 255)
+    num4 = randint(0, 255)
+    ip = str(num1)+"."+str(num2)+"."+str(num3)+"."+str(num4)
+    tuple = (ip, num1)
+    connection.send(tuple)
+    connection.close()
+
+def receiveIp(connection, connection1):
+    tuple = connection.recv()
+    connection1.send(tuple)
+    connection.close()
+    connection1.close()
+
+def ipClassifier(connection):
+    tuple = connection.recv()
+    ip, num1 = tuple
+    ipClass = "Class not identified"
+    if num1 > 0 and num1 < 128:
+        ipClass = "Class A"
+    elif num1 >= 128 and num1 < 192:
+        ipClass = "Class B"
+    elif num1 >= 192 and num1 < 224:
+        ipClass = "Class C"
+    elif num1 >= 224 and num1 < 240:
+        ipClass = "Class D"
+    elif num1 >= 240 and num1 <= 255:
+        ipClass = "Class E"
+    else:
+        ipClass = "Invalid IP"
+    print(ip + " - " + ipClass)
+
+if __name__ == "__main__":
+    left, right = Pipe()
+    left1, right1 = Pipe()
+    for i in range(50):
+        p1 = Process(target=randomIp, args=(left,))
+        p2 = Process(target=receiveIp, args=(right, left1))
+        p3 = Process(target=ipClassifier, args=(right1,))
+        p1.start()
+        p2.start()
+        p3.start()
+        p1.join()
+        p2.join()
+        p3.join()
+
+"""
+En este ejercicio vamos a lanzar varios procesos, cuyas entradas y salidas están enlazadas.
+Para ello tendremos tres procesos distintos:
+
+    Proceso 1: Va a generar 10 direcciones IP de forma aleatoria y se las enviará al Proceso 2.
+    Proceso 2: Va a leer las direcciones IP que recibe del Proceso 1 y
+               va a enviar al Proceso 3 únicamente aquellas que pertenezcan a las clases A, B o C.
+    Proceso 3: Va a leer las direcciones IP procedentes del Proceso 2 (no se sabe qué número llegarán)
+               y va a imprimir por consola la dirección IP y a continuación la clase a la que pertenece.
+	
+    Lanza los tres procesos en orden.
+"""
